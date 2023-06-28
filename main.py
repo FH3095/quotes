31e0afdcf305d92f1a53fd6da607c3830f1af606
FH3095 <FH3095@users.noreply.github.com>
2023-06-28 21:39:09 +0200

    Make main.py executable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 from ts3 import Ts3
 from discord import Discord
@@ -10,7 +11,7 @@ quote = getQuote(config)
 with open(config["QUOTE_DAY_FILE"], mode="wt", encoding = "utf-8") as quoteFile:
     quoteFile.write(quote + "\n")
 
-#Discord(config).sendQuote(quote)
+Discord(config).sendQuote(quote)
 
 for serverId in config["TS3_IDS"]:
     Ts3(config, serverId).updateQuote(quote)
